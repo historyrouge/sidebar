08fe495dc7b5269fd09d4211d47398678a35eb9d
harsh <historyrouge@gmail.com>
2025-08-29 13:30:33 +0000

    make the light mode completely new degine more beter ui and oneplus slat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,8 +18,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"Public Sans"', 'sans-serif'],
-        headline: ['"Public Sans"', 'sans-serif'],
+        body: ['"Inter"', 'sans-serif'],
+        headline: ['"Inter"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
