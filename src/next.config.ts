97715afed038e9a3edba92ee9af81255ca33b5c1
harsh <historyrouge@gmail.com>
2025-09-21 09:05:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/next.config.ts
+++ src/next.config.ts
@@ -31,7 +31,6 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-control-allow-methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ]
@@ -41,5 +40,6 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
 
 
