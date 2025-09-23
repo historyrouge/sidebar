718ede85cf3949f91563fa682aff670499aa5cc9
harsh <historyrouge@gmail.com>
2025-09-23 18:06:54 +0000

    add more professional ui degines in light mode

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-2xl border bg-card text-card-foreground shadow-[0_4px_16px_rgba(0,0,0,0.1)]",
+      "rounded-2xl border bg-card text-card-foreground shadow-md shadow-black/[0.03]",
       className
     )}
     {...props}
