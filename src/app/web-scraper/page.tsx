649a714bd8a7416b006526a190049f0ba0e02be4
harsh <historyrouge@gmail.com>
2025-10-11 18:13:58 +0000

    23:43:01.365  ⨯ useSearchParams() should be wrapped in a suspense bound

--- src/app/web-scraper/page.tsx
+++ src/app/web-scraper/page.tsx
@@ -1,12 +1,20 @@
-
 "use client";
 import { MainLayout } from "@/components/main-layout";
 import { WebBrowserContent } from '@/components/web-browser-content';
+import { Suspense } from "react";
 
-export default function WebScraperPage() {
+function WebScraperPageContent() {
     return (
         <MainLayout>
            <WebBrowserContent />
         </MainLayout>
     );
+}
+
+export default function WebScraperPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <WebScraperPageContent />
+        </Suspense>
+    )
 }
