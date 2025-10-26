fe069c3f2c40b88602f88ece2dfec7ad47d8360b
harsh <historyrouge@gmail.com>
2025-10-26 17:05:09 +0000

    goofle login not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   "appId": "1:464814044908:web:f69d16aa7c8ed63814d7c9",
   "storageBucket": "scholarsage-ue2av.firebasestorage.app",
   "apiKey": "AIzaSyBBucBQ-1aHNNna0VckiW_AqMK5CSqul8k",
-  "authDomain": "easylearnai.vercel.app",
+  "authDomain": "scholarsage-ue2av.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "464814044908"
 };
