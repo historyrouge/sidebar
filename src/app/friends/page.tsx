1a685eae2f7a52dfde74d1872e7cbf3f547290ac
harsh <historyrouge@gmail.com>
2025-10-27 17:42:06 +0000

    ok just strt

---
+++ src/app/friends/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { MainLayout } from "@/components/main-layout";
+import { FriendsContent } from '@/components/friends-content';
+
+export default function FriendsPage() {
+    return (
+        <MainLayout>
+           <FriendsContent />
+        </MainLayout>
+    );
+}
--- src/components/friends-content.tsx
+++ src/components/friends-content.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { Users } from "lucide-react";
